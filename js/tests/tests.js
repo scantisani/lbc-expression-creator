22c11ad54813db42216ef2be9c65237b883415bf
Scott Cantisani <scantisani@gmail.com>
2015-12-10 23:30:26 +0000

    Added tests for parsing every available comparison operator

--- js/tests/tests.js
+++ js/tests/tests.js
@@ -8,7 +8,8 @@ require.config({
 
 require(['parsing/lbc-parser', 'app']);
 
-QUnit.test( "parse test", function( assert ) {
+QUnit.module("Operator parsing tests");
+QUnit.test("Greater Than operator parses as expected", function(assert) {
   var tree = {
     "tag": "Expression",
     "children": [
@@ -63,3 +64,288 @@ QUnit.test( "parse test", function( assert ) {
   QUnit.dump.maxDepth = 20;
   assert.deepEqual(parseExpression('Future Concentration X > 5'), tree, "Expression 'Future Concentration X > 5' parses correctly");
 });
+
+QUnit.test( "Less Than operator functions as expected", function(assert) {
+  var tree = {
+    "tag": "Expression",
+    "children": [
+      {
+        "tag": "Future",
+        "children": []
+      },
+      {
+        "tag": "Conditional",
+        "children": [
+          {
+            "tag": "Concentration",
+            "children": [
+              {
+                "tag": "Concentration_Op",
+                "children": []
+              },
+              {
+                "tag": "Species",
+                "children": [
+                  {
+                    "tag": [
+                      "X"
+                    ],
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "tag": "<",
+            "children": []
+          },
+          {
+            "tag": "Real",
+            "children": [
+              {
+                "tag": [
+                  "5"
+                ],
+                "children": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  // increase max depth so the diff is visible in case of error
+  QUnit.dump.maxDepth = 20;
+  assert.deepEqual(parseExpression('Future Concentration X < 5'), tree, "Expression 'Future Concentration X < 5' parses correctly");
+});
+
+QUnit.test("Equality operator parses as expected", function(assert) {
+  var tree = {
+    "tag": "Expression",
+    "children": [
+      {
+        "tag": "Future",
+        "children": []
+      },
+      {
+        "tag": "Conditional",
+        "children": [
+          {
+            "tag": "Concentration",
+            "children": [
+              {
+                "tag": "Concentration_Op",
+                "children": []
+              },
+              {
+                "tag": "Species",
+                "children": [
+                  {
+                    "tag": [
+                      "X"
+                    ],
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "tag": "=",
+            "children": []
+          },
+          {
+            "tag": "Real",
+            "children": [
+              {
+                "tag": [
+                  "5"
+                ],
+                "children": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  // increase max depth so the diff is visible in case of error
+  QUnit.dump.maxDepth = 20;
+  assert.deepEqual(parseExpression('Future Concentration X = 5'), tree, "Expression 'Future Concentration X = 5' parses correctly");
+});
+
+QUnit.test( "Not Equal operator parses as expected", function(assert) {
+  var tree = {
+    "tag": "Expression",
+    "children": [
+      {
+        "tag": "Future",
+        "children": []
+      },
+      {
+        "tag": "Conditional",
+        "children": [
+          {
+            "tag": "Concentration",
+            "children": [
+              {
+                "tag": "Concentration_Op",
+                "children": []
+              },
+              {
+                "tag": "Species",
+                "children": [
+                  {
+                    "tag": [
+                      "X"
+                    ],
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "tag": "!=",
+            "children": []
+          },
+          {
+            "tag": "Real",
+            "children": [
+              {
+                "tag": [
+                  "5"
+                ],
+                "children": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  // increase max depth so the diff is visible in case of error
+  QUnit.dump.maxDepth = 20;
+  assert.deepEqual(parseExpression('Future Concentration X != 5'), tree, "Expression 'Future Concentration X != 5' parses correctly");
+});
+
+QUnit.test("Greater Than or Equal To operator functions as expected", function(assert) {
+  var tree = {
+    "tag": "Expression",
+    "children": [
+      {
+        "tag": "Future",
+        "children": []
+      },
+      {
+        "tag": "Conditional",
+        "children": [
+          {
+            "tag": "Concentration",
+            "children": [
+              {
+                "tag": "Concentration_Op",
+                "children": []
+              },
+              {
+                "tag": "Species",
+                "children": [
+                  {
+                    "tag": [
+                      "X"
+                    ],
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "tag": ">=",
+            "children": []
+          },
+          {
+            "tag": "Real",
+            "children": [
+              {
+                "tag": [
+                  "5"
+                ],
+                "children": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  // increase max depth so the diff is visible in case of error
+  QUnit.dump.maxDepth = 20;
+  assert.deepEqual(parseExpression('Future Concentration X >= 5'), tree, "Expression 'Future Concentration X >= 5' parses correctly");
+});
+
+QUnit.test("Less Than Or Equal operator parses as expected", function(assert) {
+  var tree = {
+    "tag": "Expression",
+    "children": [
+      {
+        "tag": "Future",
+        "children": []
+      },
+      {
+        "tag": "Conditional",
+        "children": [
+          {
+            "tag": "Concentration",
+            "children": [
+              {
+                "tag": "Concentration_Op",
+                "children": []
+              },
+              {
+                "tag": "Species",
+                "children": [
+                  {
+                    "tag": [
+                      "X"
+                    ],
+                    "children": []
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "tag": "<=",
+            "children": []
+          },
+          {
+            "tag": "Real",
+            "children": [
+              {
+                "tag": [
+                  "5"
+                ],
+                "children": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  // increase max depth so the diff is visible in case of error
+  QUnit.dump.maxDepth = 20;
+  assert.deepEqual(parseExpression('Future Concentration X <= 5'), tree, "Expression 'Future Concentration X <= 5' parses correctly");
+});
+
+// QUnit.module("Syntax tree to LBC translation tests");
+// QUnit.test("", function(assert) {
+
+// });
