5aceb341483722a5e529eb96f7960f3968d6e5c9
Scott Cantisani <scantisani@gmail.com>
2015-12-14 01:32:59 +0000

    Blocks now directly generate trees instead of expressions for parser

--- js/blockly/generators/javascript/lbc.js
+++ js/blockly/generators/javascript/lbc.js
@@ -11,20 +11,33 @@ goog.require('Blockly.JavaScript');
 
 Blockly.JavaScript['lbc_concentration'] = function(block) {
   var species = block.getFieldValue('SPECIES');
-  var code = 'Concentration ' + species;
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  var code = JSON.stringify({
+    tag: 'Concentration',
+    value: species
+  });
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['lbc_future'] = function(block) {
   var expression = Blockly.JavaScript.valueToCode(block, 'EXPRESSION', Blockly.JavaScript.ORDER_NONE);
-  var code = 'Future ' + expression;
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  var code = '{' +
+    '"tag": "TempComp",' + 
+    '"children": [' +
+      '{"tag": "Future"},' +
+      expression +
+    ']}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['lbc_global'] = function(block) {
   var expression = Blockly.JavaScript.valueToCode(block, 'EXPRESSION', Blockly.JavaScript.ORDER_NONE);
-  var code = 'Global ' + expression;
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  var code = '{' +
+    '"tag": "Expression",' + 
+    '"children": [' +
+      '{"tag": "Global"},' +
+      expression +
+    ']}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['lbc_compare'] = function(block) {
@@ -38,15 +51,19 @@ Blockly.JavaScript['lbc_compare'] = function(block) {
   };
 
   var operator = block.getFieldValue('OP');
-  var order = (operator == 'EQ' || operator == 'NEQ') ?
-      Blockly.JavaScript.ORDER_EQUALITY : Blockly.JavaScript.ORDER_RELATIONAL;
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', order) || '0';
-  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', order) || '0';
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_NONE) || '';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_NONE) || '';
 
   var comparison = OPERATORS[operator];
 
-  var code = argument0 + ' ' + comparison + ' ' + argument1;
-  return [code, order];
+  var code = '{' +
+    '"tag": "Comparison",' +
+    '"children": [' +
+      argument0 + ',' +
+      '{"tag": "Comparison_Op", "value": "' + comparison + '"},' +
+      argument1 +
+    ']}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['lbc_temporal_compare'] = function(block) {
@@ -65,20 +82,29 @@ Blockly.JavaScript['lbc_temporal_compare'] = function(block) {
   };
 
   var operator = block.getFieldValue('OP');
-  var order = (operator == 'EQ' || operator == 'NEQ') ?
-      Blockly.JavaScript.ORDER_EQUALITY : Blockly.JavaScript.ORDER_RELATIONAL;
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', order) || '0';
-  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', order) || '0';
   var temporal = block.getFieldValue('TEMP');
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_NONE) || '';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_NONE) || '';
 
-  var comparison = OPERATORS[operator];
+  operator = OPERATORS[operator];
   temporal = TEMP_MODALITIES[temporal];
 
-  var code = temporal + ' ' + argument0 + ' ' + comparison + ' ' + argument1;
-  return [code, order];
+  var code = '{' +
+    '"tag": "TempMidComp",' +
+    '"children": [' +
+      '{"tag": "' + temporal + '"},' +
+      '{"tag": "Comparison",' +
+        '"children": [' +
+          argument0 + ',' +
+          '{"tag": "Comparison_Op", "value": "' + operator + '"},' +
+          argument1 +
+      ']}' +
+    ']}';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['lbc_real'] = function(block) {
   var num = block.getFieldValue('NUM');
-  return [num, Blockly.JavaScript.ORDER_ATOMIC];
+  var code = '{"tag": "Real", "value": "' + num + '"}';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
