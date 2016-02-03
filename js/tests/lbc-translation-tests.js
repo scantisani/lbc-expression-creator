e6fdff5d200c94c37f10e5047ac3c73bb9f0d75e
Scott Cantisani <scantisani@gmail.com>
2016-02-03 01:47:13 +0000

    Merged two tree translation test modules

--- js/tests/lbc-translation-tests.js
+++ js/tests/lbc-translation-tests.js
@@ -1,4 +1,4 @@
-QUnit.module("Tree -> LBC");
+QUnit.module("Tree -> LBC, English");
 QUnit.test("Expr1 is translated correctly", function(assert) {
   var tree = {
     tag: 'Expr1',
@@ -27,6 +27,7 @@ QUnit.test("Expr1 is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'F([A] > 0)');
+  assert.equal(treeToEnglish(tree), 'The concentration of A is eventually greater than 0.');
 });
 
 QUnit.test("Expr2 is translated correctly", function(assert) {
@@ -55,6 +56,7 @@ QUnit.test("Expr2 is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'F([A] > 5)');
+  assert.equal(treeToEnglish(tree), 'The concentration of A is eventually greater than 5.');
 });
 
 QUnit.test("Expr3 is translated correctly", function(assert) {
@@ -77,6 +79,7 @@ QUnit.test("Expr3 is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'F(G([A] < 5))');
+  assert.equal(treeToEnglish(tree), 'The concentration of A eventually drops to and stays below 5.');
 });
 
 QUnit.test("Expr4 is translated correctly", function(assert) {
@@ -120,9 +123,11 @@ QUnit.test("Expr4 is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'G{5, 15}([B] = 0.75)');
+  assert.equal(treeToEnglish(tree), 'Between times 5 and 15, the concentration of B is always equal to 0.75.');
 
   tree.children[0].children[0].tag = 'Future';
   assert.equal(treeToLBC(tree), 'F{5, 15}([B] = 0.75)');
+  assert.equal(treeToEnglish(tree), 'At some point between times 5 and 15, the concentration of B is equal to 0.75.');
 });
 
 QUnit.test("Real values are translated correctly", function(assert) {
@@ -132,6 +137,7 @@ QUnit.test("Real values are translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), '5');
+  assert.equal(treeToEnglish(tree), '5');
 });
 
 QUnit.test("Concentrations are translated correctly", function(assert) {
@@ -140,12 +146,14 @@ QUnit.test("Concentrations are translated correctly", function(assert) {
     value: 'A'
   };
   assert.equal(treeToLBC(tree), '[A]');
+  assert.equal(treeToEnglish(tree), 'the concentration of A');
 
   tree = {
     tag: 'Concentration',
     value: 'phos'
   };
   assert.equal(treeToLBC(tree), '[phos]');
+  assert.equal(treeToEnglish(tree), 'the concentration of phos');
 });
 
 QUnit.test("Global is translated correctly", function(assert) {
@@ -154,6 +162,7 @@ QUnit.test("Global is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'G');
+  assert.equal(treeToEnglish(tree), 'always');
 });
 
 QUnit.test("Future is translated correctly", function(assert) {
@@ -162,6 +171,7 @@ QUnit.test("Future is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), 'F');
+  assert.equal(treeToEnglish(tree), 'eventually');
 });
 
 QUnit.test("Comparison is translated correctly", function(assert) {
@@ -184,6 +194,7 @@ QUnit.test("Comparison is translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), '[X] > 5');
+  assert.equal(treeToEnglish(tree), 'the concentration of X is greater than 5');
 });
 
 QUnit.test("Comparison operators are translated correctly", function(assert) {
@@ -192,24 +203,28 @@ QUnit.test("Comparison operators are translated correctly", function(assert) {
     value: '>'
   };
   assert.equal(treeToLBC(tree), '>');
+  assert.equal(treeToEnglish(tree), 'greater than');
 
   tree = {
     tag: 'ComparisonOp',
     value: '<'
   };
   assert.equal(treeToLBC(tree), '<');
+  assert.equal(treeToEnglish(tree), 'less than');
 
   tree = {
     tag: 'ComparisonOp',
     value: '='
   };
   assert.equal(treeToLBC(tree), '=');
+  assert.equal(treeToEnglish(tree), 'equal to');
 
   tree = {
     tag: 'ComparisonOp',
     value: '!='
   };
   assert.equal(treeToLBC(tree), '!=');
+  assert.equal(treeToEnglish(tree), 'not equal to');
 
 });
 
@@ -232,13 +247,19 @@ QUnit.test("Non-recursive arithmetic is translated correctly", function(assert) 
     ]
   };
   assert.equal(treeToLBC(tree), '([A] + 15)');
+  assert.equal(treeToEnglish(tree), 'the concentration of A plus 15');
 
   tree.children[1].value = '-';
   assert.equal(treeToLBC(tree), '([A] - 15)');
+  assert.equal(treeToEnglish(tree), 'the concentration of A minus 15');
+
   tree.children[1].value = '*';
   assert.equal(treeToLBC(tree), '([A] * 15)');
+  assert.equal(treeToEnglish(tree), 'the concentration of A multiplied by 15');
+
   tree.children[1].value = '/';
   assert.equal(treeToLBC(tree), '([A] / 15)');
+  assert.equal(treeToEnglish(tree), 'the concentration of A divided by 15');
 
   tree = {
     tag: 'Arithmetic',
@@ -258,13 +279,19 @@ QUnit.test("Non-recursive arithmetic is translated correctly", function(assert) 
     ]
   };
   assert.equal(treeToLBC(tree), '([V] + [T])');
+  assert.equal(treeToEnglish(tree), 'the concentration of V plus the concentration of T');
 
   tree.children[1].value = '-';
   assert.equal(treeToLBC(tree), '([V] - [T])');
+  assert.equal(treeToEnglish(tree), 'the concentration of V minus the concentration of T');
+
   tree.children[1].value = '*';
   assert.equal(treeToLBC(tree), '([V] * [T])');
+  assert.equal(treeToEnglish(tree), 'the concentration of V multiplied by the concentration of T');
+
   tree.children[1].value = '/';
   assert.equal(treeToLBC(tree), '([V] / [T])');
+  assert.equal(treeToEnglish(tree), 'the concentration of V divided by the concentration of T');
 });
 
 QUnit.test("Recursive arithmetic is translated correctly", function(assert) {
@@ -299,13 +326,19 @@ QUnit.test("Recursive arithmetic is translated correctly", function(assert) {
     ]
   };
   assert.equal(treeToLBC(tree), '([A] + ([B] + 15))');
+  assert.equal(treeToEnglish(tree), 'the concentration of A, plus the concentration of B plus 15');
 
   tree.children[1].value = '-';
   assert.equal(treeToLBC(tree), '([A] - ([B] + 15))');
+  assert.equal(treeToEnglish(tree), 'the concentration of A, minus the concentration of B plus 15');
+
   tree.children[1].value = '*';
   assert.equal(treeToLBC(tree), '([A] * ([B] + 15))');
+  assert.equal(treeToEnglish(tree), 'the concentration of A, multiplied by the concentration of B plus 15');
+
   tree.children[1].value = '/';
   assert.equal(treeToLBC(tree), '([A] / ([B] + 15))');
+  assert.equal(treeToEnglish(tree), 'the concentration of A, divided by the concentration of B plus 15');
 
   tree = {
     tag: 'Arithmetic',
@@ -338,13 +371,19 @@ QUnit.test("Recursive arithmetic is translated correctly", function(assert) {
     ]
   };
   assert.equal(treeToLBC(tree), '([V] + ([S] + [T]))');
+  assert.equal(treeToEnglish(tree), 'the concentration of V, plus the concentration of S plus the concentration of T');
 
   tree.children[1].value = '-';
   assert.equal(treeToLBC(tree), '([V] - ([S] + [T]))');
+  assert.equal(treeToEnglish(tree), 'the concentration of V, minus the concentration of S plus the concentration of T');
+
   tree.children[1].value = '*';
   assert.equal(treeToLBC(tree), '([V] * ([S] + [T]))');
+  assert.equal(treeToEnglish(tree), 'the concentration of V, multiplied by the concentration of S plus the concentration of T');
+
   tree.children[1].value = '/';
   assert.equal(treeToLBC(tree), '([V] / ([S] + [T]))');
+  assert.equal(treeToEnglish(tree), 'the concentration of V, divided by the concentration of S plus the concentration of T');
 });
 
 QUnit.test("Comments are translated correctly", function(assert) {
@@ -354,4 +393,5 @@ QUnit.test("Comments are translated correctly", function(assert) {
   };
 
   assert.equal(treeToLBC(tree), '"a placeholder for other blocks"');
+  assert.equal(treeToEnglish(tree), '"a placeholder for other blocks"');
 });
